d497c452c1cb805a81d08e302f74de8f0f1d1e61
Kevin Chen <kevinchen@Kevins-MacBook-Pro.local>
2019-01-19 09:50:00 +0700

    remove force ssl

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+/*
 //= =======================================
 // force SSL when in production
 //= =======================================
@@ -30,6 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   
   app.use(forceSSL());
 }
+*/
 
 //= =======================================
 // connect to Mongodb
